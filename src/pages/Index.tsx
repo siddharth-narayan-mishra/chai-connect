753db835f734f13afbc32a2c9ba7896554234735
siddharth <siddharthnmishra7@gmail.com>
2025-11-09 21:10:13 +0530

    final commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import SkillExchange from "@/components/SkillExchange";
 import Mentorship from "@/components/Mentorship";
